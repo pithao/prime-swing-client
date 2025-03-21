4bde92ce610270affee76efff48aa668ec78aa30
Brian Larson <brian@brianlarson.com>
2025-03-20 21:26:41 -0500

    Restyle footer with MUI components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,80 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import LanguageIcon from "@mui/icons-material/Language";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" sx={{ color: "text.secondary", mt: 1 }}>
+      {"Copyright © "}
+      <Link color="text.secondary" href="#">
+        Sitemark
+      </Link>{" "}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
 function Footer() {
   return (
-    <footer>
-      <p>Copyright &copy; {new Date().getFullYear()}</p>
-    </footer>
+    <>
+      <Container
+        maxWidth="xl"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: { xs: 4, sm: 8 },
+          py: { xs: 8, sm: 10 },
+          textAlign: { sm: "center", md: "left" },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            pt: { xs: 4, sm: 8 },
+            width: "100%",
+            borderTop: "1px solid",
+            borderColor: "divider",
+          }}
+        >
+          <div>
+            <Link color="text.secondary" variant="body2" href="#">
+              Privacy Policy
+            </Link>
+            <Typography sx={{ display: "inline", mx: 0.5, opacity: 0.5 }}>
+              &nbsp;•&nbsp;
+            </Typography>
+            <Link color="text.secondary" variant="body2" href="#">
+              Terms of Service
+            </Link>
+            <Copyright />
+          </div>
+          <Stack
+            direction="row"
+            spacing={1}
+            useFlexGap
+            sx={{ justifyContent: "left", color: "text.secondary" }}
+          >
+            <IconButton
+              color="inherit"
+              size="small"
+              href="#"
+              aria-label="Visit our website"
+              target="_blank"
+              rel="noopener noreferrer"
+              sx={{ alignSelf: "center" }}
+            >
+              <LanguageIcon />
+            </IconButton>
+          </Stack>
+        </Box>
+      </Container>
+    </>
   );
 }
 
