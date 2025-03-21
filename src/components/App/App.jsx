d51765d310da2090d3060bc9e4049e9b392e9e70
Brian Larson <brian@brianlarson.com>
2025-03-20 19:39:46 -0500

    Add MUI Box wrapper element, prettier auto-format changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,14 @@
-import { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import useStore from '../../zustand/store';
-import Nav from '../Nav/Nav';
-import HomePage from '../HomePage/HomePage';
-import LoginPage from '../LoginPage/LoginPage';
-import RegisterPage from '../RegisterPage/RegisterPage';
-import ClassSurvey from "../ClassSurvey/ClassSurvey"; 
+import useStore from "../../zustand/store";
+import Nav from "../Nav/Nav";
+import HomePage from "../HomePage/HomePage";
+import LoginPage from "../LoginPage/LoginPage";
+import RegisterPage from "../RegisterPage/RegisterPage";
+import ClassSurvey from "../ClassSurvey/ClassSurvey";
 
+import { Box, Container, AppBar, Toolbar, Typography } from "@mui/material";
 
 function App() {
   const user = useStore((state) => state.user);
@@ -22,15 +19,16 @@ function App() {
   }, [fetchUser]);
 
   return (
-    <>
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <header>
         <h1>Prime Solo Project</h1>
         <Nav />
       </header>
       <main>
         <Routes>
-          <Route 
-            exact path="/"
+          <Route
+            exact
+            path="/"
             element={
               user.id ? (
                 <HomePage /> // Render HomePage for authenticated user.
@@ -39,8 +37,9 @@ function App() {
               )
             }
           />
-          <Route 
-            exact path="/login"
+          <Route
+            exact
+            path="/login"
             element={
               user.id ? (
                 <Navigate to="/" replace /> // Redirect authenticated user.
@@ -49,8 +48,9 @@ function App() {
               )
             }
           />
-          <Route 
-            exact path="/registration"
+          <Route
+            exact
+            path="/registration"
             element={
               user.id ? (
                 <Navigate to="/" replace /> // Redirect authenticated user.
@@ -59,32 +59,39 @@ function App() {
               )
             }
           />
-          <Route 
-            exact path="/about"
+          <Route
+            exact
+            path="/about"
             element={
               <>
                 <h2>About Page</h2>
                 <p>
-                  Intelligence doesn’t seem like an aspect of personal character, and it isn’t.
-                  Coincidentally, great intelligence is only loosely connected to being a good programmer.
+                  Intelligence doesn’t seem like an aspect of personal
+                  character, and it isn’t. Coincidentally, great intelligence is
+                  only loosely connected to being a good programmer.
+                </p>
+                <p>What? You don’t have to be superintelligent?</p>
+                <p>
+                  No, you don’t. Nobody is really smart enough to program
+                  computers. Fully understanding an average program requires an
+                  almost limitless capacity to absorb details and an equal
+                  capacity to comprehend them all at the same time. The way you
+                  focus your intelligence is more important than how much
+                  intelligence you have…
                 </p>
                 <p>
-                  What? You don’t have to be superintelligent?
-                </p>
-                <p>
-                  No, you don’t. Nobody is really smart enough to program computers.
-                  Fully understanding an average program requires an almost limitless capacity
-                  to absorb details and an equal capacity to comprehend them all at the same time.
-                  The way you focus your intelligence is more important than how much intelligence you have…
-                </p>
-                <p>
-                  …most of programming is an attempt to compensate for the strictly limited size of our skulls.
-                  The people who are the best programmers are the people who realize how small their brains are.
-                  They are humble. The people who are the worst at programming are the people who refuse to
-                  accept the fact that their brains aren’t equal to the task.
-                  Their egos keep them from being great programmers.
-                  The more you learn to compensate for your small brain, the better a programmer you’ll be.
-                  <span className="squiggle"> The more humble you are, the faster you’ll improve.</span>
+                  …most of programming is an attempt to compensate for the
+                  strictly limited size of our skulls. The people who are the
+                  best programmers are the people who realize how small their
+                  brains are. They are humble. The people who are the worst at
+                  programming are the people who refuse to accept the fact that
+                  their brains aren’t equal to the task. Their egos keep them
+                  from being great programmers. The more you learn to compensate
+                  for your small brain, the better a programmer you’ll be.
+                  <span className="squiggle">
+                    {" "}
+                    The more humble you are, the faster you’ll improve.
+                  </span>
                 </p>
                 <p>
                   --From Steve McConnell's <em>Code Complete</em>.
@@ -92,24 +99,15 @@ function App() {
               </>
             }
           />
-          <Route
-            path="*"
-            element={
-              <h2>404 Page</h2>
-            } 
-          />
-<Route 
-  exact path="/class-survey"
-  element={<ClassSurvey />}
-/>
+          <Route path="*" element={<h2>404 Page</h2>} />
+          <Route exact path="/class-survey" element={<ClassSurvey />} />
         </Routes>
       </main>
       <footer>
         <p>Copyright © {new Date().getFullYear()}</p>
       </footer>
-    </>
+    </Box>
   );
 }
-
 
 export default App;
