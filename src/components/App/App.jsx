6afd717de3a3c9fa4607e4a7183223d7d741e7cd
Brian Larson <brian@brianlarson.com>
2025-03-26 21:14:12 -0500

    Add logo and theme colors, handle MUI theme caching

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,9 +16,22 @@ import LocationSurvey from "../LocationSurvey/LocationSurvey";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 function App() {
   const user = useStore((state) => state.user);
   const fetchUser = useStore((state) => state.fetchUser);
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#000000",
+      },
+      secondary: {
+        main: "#c20020",
+      },
+    },
+  });
 
   useEffect(() => {
     fetchUser();
@@ -26,90 +39,93 @@ function App() {
 
   return (
     <CssBaseline>
-      <Header />
-      <Box component="main" style={{ marginTop: "3rem", padding: "0 .5rem" }}>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              user.id ? (
-                <HomePage /> // Render HomePage for authenticated user.
-              ) : (
-                <Navigate to="/login" replace /> // Redirect unauthenticated user.
-              )
-            }
-          />
-          <Route
-            exact
-            path="/login"
-            element={
-              user.id ? (
-                <Navigate to="/" replace /> // Redirect authenticated user.
-              ) : (
-                <LoginPage /> // Render LoginPage for unauthenticated user.
-              )
-            }
-          />
-          <Route
-            exact
-            path="/registration"
-            element={
-              user.id ? (
-                <Navigate to="/" replace /> // Redirect authenticated user.
-              ) : (
-                <RegisterPage /> // Render RegisterPage for unauthenticated user.
-              )
-            }
-          />
-          <Route
-            exact
-            path="/about"
-            element={
-              <>
-                <h2>About Page</h2>
-                <p>
-                  Intelligence doesn’t seem like an aspect of personal
-                  character, and it isn’t. Coincidentally, great intelligence is
-                  only loosely connected to being a good programmer.
-                </p>
-                <p>What? You don’t have to be superintelligent?</p>
-                <p>
-                  No, you don’t. Nobody is really smart enough to program
-                  computers. Fully understanding an average program requires an
-                  almost limitless capacity to absorb details and an equal
-                  capacity to comprehend them all at the same time. The way you
-                  focus your intelligence is more important than how much
-                  intelligence you have…
-                </p>
-                <p>
-                  …most of programming is an attempt to compensate for the
-                  strictly limited size of our skulls. The people who are the
-                  best programmers are the people who realize how small their
-                  brains are. They are humble. The people who are the worst at
-                  programming are the people who refuse to accept the fact that
-                  their brains aren’t equal to the task. Their egos keep them
-                  from being great programmers. The more you learn to compensate
-                  for your small brain, the better a programmer you’ll be.
-                  <span className="squiggle">
-                    {" "}
-                    The more humble you are, the faster you’ll improve.
-                  </span>
-                </p>
-                <p>
-                  --From Steve McConnell's <em>Code Complete</em>.
-                </p>
-              </>
-            }
-          />
-          <Route exact path="/class-survey" element={<ClassSurvey />} />
-          <Route exact path="/event-survey" element={<EventSurvey />} />
-          <Route exact path="/dance-survey" element={<DanceSurvey />} />
-          <Route exact path="/location-survey" element={<LocationSurvey />} />
-          <Route path="*" element={<h2>404 Page</h2>} />
-        </Routes>
-      </Box>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Box component="main" style={{ marginTop: "3rem", padding: "0 .5rem" }}>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                user.id ? (
+                  <HomePage /> // Render HomePage for authenticated user.
+                ) : (
+                  <Navigate to="/login" replace /> // Redirect unauthenticated user.
+                )
+              }
+            />
+            <Route
+              exact
+              path="/login"
+              element={
+                user.id ? (
+                  <Navigate to="/" replace /> // Redirect authenticated user.
+                ) : (
+                  <LoginPage /> // Render LoginPage for unauthenticated user.
+                )
+              }
+            />
+            <Route
+              exact
+              path="/registration"
+              element={
+                user.id ? (
+                  <Navigate to="/" replace /> // Redirect authenticated user.
+                ) : (
+                  <RegisterPage /> // Render RegisterPage for unauthenticated user.
+                )
+              }
+            />
+            <Route
+              exact
+              path="/about"
+              element={
+                <>
+                  <h2>About Page</h2>
+                  <p>
+                    Intelligence doesn’t seem like an aspect of personal
+                    character, and it isn’t. Coincidentally, great intelligence
+                    is only loosely connected to being a good programmer.
+                  </p>
+                  <p>What? You don’t have to be superintelligent?</p>
+                  <p>
+                    No, you don’t. Nobody is really smart enough to program
+                    computers. Fully understanding an average program requires
+                    an almost limitless capacity to absorb details and an equal
+                    capacity to comprehend them all at the same time. The way
+                    you focus your intelligence is more important than how much
+                    intelligence you have…
+                  </p>
+                  <p>
+                    …most of programming is an attempt to compensate for the
+                    strictly limited size of our skulls. The people who are the
+                    best programmers are the people who realize how small their
+                    brains are. They are humble. The people who are the worst at
+                    programming are the people who refuse to accept the fact
+                    that their brains aren’t equal to the task. Their egos keep
+                    them from being great programmers. The more you learn to
+                    compensate for your small brain, the better a programmer
+                    you’ll be.
+                    <span className="squiggle">
+                      {" "}
+                      The more humble you are, the faster you’ll improve.
+                    </span>
+                  </p>
+                  <p>
+                    --From Steve McConnell's <em>Code Complete</em>.
+                  </p>
+                </>
+              }
+            />
+            <Route exact path="/class-survey" element={<ClassSurvey />} />
+            <Route exact path="/event-survey" element={<EventSurvey />} />
+            <Route exact path="/dance-survey" element={<DanceSurvey />} />
+            <Route exact path="/location-survey" element={<LocationSurvey />} />
+            <Route path="*" element={<h2>404 Page</h2>} />
+          </Routes>
+        </Box>
+        <Footer />
+      </ThemeProvider>
     </CssBaseline>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -51,24 +51,19 @@ function Header() {
     >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
+          <Box
+            href="/"
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
             }}
           >
-            LOGO
-          </Typography>
+            <img
+              src="/images/rebels-logo.png"
+              style={{ width: "7rem", padding: "1rem" }}
+              alt="Twin Cities Rebels Swing Dance Club"
+            />
+          </Box>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
@@ -104,25 +99,7 @@ function Header() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
+
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
