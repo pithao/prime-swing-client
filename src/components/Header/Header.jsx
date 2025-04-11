aa3413400744ae9cc7f5dfe101bb66fe49154ef3
Brian Larson <brian@brianlarson.com>
2025-04-10 20:40:52 -0500

    Hide admin nav for now

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -160,7 +160,7 @@ function Header() {
           </Box>
 
           {/* Icon/avatar nav link - utility for admins */}
-          <Box sx={{ flexGrow: 0 }}>
+          {/* <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
@@ -190,7 +190,7 @@ function Header() {
                 </MenuItem>
               ))}
             </Menu>
-          </Box>
+          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
