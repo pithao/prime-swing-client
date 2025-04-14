81ef005e3b3c4e574db57235c7bc3cba907ca3ec
Zoe Copoulos <zoe.copoulos@gmail.com>
2025-04-13 19:31:21 -0500

    modified server js to remove erroneous sql routers

--- server/server.js
+++ server/server.js
@@ -13,9 +13,6 @@ const passport = require('./strategies/user.strategy');
 
 // Require router files:
 const userRouter = require('./routes/user.router');
-const eventformdataRouter = require('./routes/eventformdata.router');
-const danceformdataRouter = require('./routes/danceformdata.router');
-const classformdataRouter = require('./routes/classformdata.router');
 
 // Apply middleware:
 app.use(express.json());
@@ -27,9 +24,6 @@ app.use(passport.session());
 
 // Apply router files:
 app.use('/api/user', userRouter);
-app.use('/api/eventformdata', eventformdataRouter); 
-app.use('/api/danceformdata', danceformdataRouter);
-app.use('/api/classformdata', classformdataRouter);
 
 // Start the server:
 app.listen(PORT, () => {
